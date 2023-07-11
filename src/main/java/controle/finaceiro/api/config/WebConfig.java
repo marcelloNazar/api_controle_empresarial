34cd0272bb2d5ffaa2af0e1581061d23d5c00299
Marcello Nazar <marcellonazarb@hotmail.com>
2023-07-11 16:22:43 -0300

    alterando cors

--- src/main/java/controle/finaceiro/api/config/WebConfig.java
+++ src/main/java/controle/finaceiro/api/config/WebConfig.java
@@ -12,7 +12,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:3000")
+                .allowedOrigins("http://localhost:3000", "https://apicontroleempresarial-production.up.railway.app/")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS",  "HEAD", "TRACE", "CONNECT");
     }
 }
