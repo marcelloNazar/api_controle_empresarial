70fc9775f240ba207efb1a9ea7c38e6cd6341fac
Marcello Nazar <marcellonazarb@hotmail.com>
2023-07-11 17:07:25 -0300

    alterando cors

--- src/main/java/controle/finaceiro/api/doc/SpringDocConfig.java
+++ src/main/java/controle/finaceiro/api/doc/SpringDocConfig.java
@@ -4,13 +4,18 @@ package controle.finaceiro.api.doc;
 import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.security.SecurityScheme;
+import jakarta.annotation.PostConstruct;
 import org.springdoc.core.models.GroupedOpenApi;
 import org.springdoc.core.properties.SwaggerUiConfigParameters;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class SpringDocConfig {
+
+    @Autowired
+    private SwaggerUiConfigParameters configParameters;
 
     @Bean
     public OpenAPI customOpenAPI() {
@@ -25,10 +30,9 @@ public class SpringDocConfig {
         return GroupedOpenApi.builder().group("public").pathsToMatch("/**").build();
     }
 
-    @Bean
-    public void addPublicGroup(SwaggerUiConfigParameters configParameters) {
+    @PostConstruct
+    public void addPublicGroup() {
         configParameters.addGroup("public");
     }
-
 
 }
