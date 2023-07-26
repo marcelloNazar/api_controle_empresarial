9835fda5d27d6fa98ed9353cd01aaee4d82e8da8
Marcello Nazar <marcellonazarb@hotmail.com>
2023-07-26 15:32:28 -0300

    alterando cors

--- src/main/java/controle/finaceiro/api/domain/transacao/TransacaoDTO.java
+++ src/main/java/controle/finaceiro/api/domain/transacao/TransacaoDTO.java
@@ -6,7 +6,7 @@ import java.time.LocalDate;
 
 @Data
 public class TransacaoDTO {
-    private Boolean tipo;
+    private Boolean despesa;
     private Double valor;
     private String descricao;
     private Boolean pago;
